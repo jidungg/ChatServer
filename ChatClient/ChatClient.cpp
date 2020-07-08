dd9c5ac47cde6310cb0d4d1b283b37c53968bd2a
김지완 <akrhwkwk@gmail.com>
2020-07-09 08:52:11 +0900

    InitialCommit

---
+++ ChatClient/ChatClient.cpp
@@ -0,0 +1,50 @@
+﻿// ChatClient.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32")
+
+#define PORT 4578
+#define PACKET_SIZE 1024
+#define SERVER_IP "192.168.0.17"
+
+int main()
+{
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    SOCKET hSocket;
+    hSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    SOCKADDR_IN tAddr = {};
+    tAddr.sin_family = AF_INET;
+    tAddr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVER_IP,&tAddr.sin_addr.s_addr);
+
+    connect(hSocket, (SOCKADDR*)&tAddr, sizeof(tAddr));
+
+    char cMsg[] = "Client Send";
+    send(hSocket, cMsg, strlen(cMsg), 0);
+
+    char cBuffer[PACKET_SIZE] = {};
+    recv(hSocket, cBuffer, PACKET_SIZE, 0);
+    printf("Recv Msg: %s\n", cBuffer);
+
+    closesocket(hSocket);
+	WSACleanup();
+
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
